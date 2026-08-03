9c0d5f7441489a91f4d4260b577390be53433527
Jonnycp <jonathan-caputo@hotmail.com>
2026-08-03 12:09:36 +0200

    Routes base

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./pages/Home.jsx";
+
 const Game = () => {
-    return (
-        <h1>Speed Quiz</h1>
-    );
-}
+  return (
+    <BrowserRouter>
+      <main className="bg-background">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
+};
 
 export default Game;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (
+        <h1 className="text-primary">Home</h1>
+    );
+}
+
+export default Home;
