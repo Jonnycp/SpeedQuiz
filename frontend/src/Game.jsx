01e24649f53bd843d9443b1ee8cc0d4ba5925b91
Luca <lugenba@gmail.com>
2026-08-04 19:20:44 +0200

    commit generico

--- frontend/src/Game.jsx
+++ frontend/src/Game.jsx
@@ -1,6 +1,8 @@
 import { Icon } from "@iconify/react";
 import { BrowserRouter, Route, Routes } from "react-router";
+
 import Home from "./pages/Home.jsx";
+import Lobby from "./pages/Lobby.jsx";
 
 const Game = () => {
   return (
@@ -10,6 +12,7 @@ const Game = () => {
         <div className="absolute top-160 right-5 md:top-130 md:right-10 text-9xl -rotate-12 select-none pointer-events-none"><Icon icon="noto:party-popper"/></div>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/lobby" element={<Lobby />} />
         </Routes>
       </main>
       <footer className="text-white/50 font-primary text-sm text-center my-5 font-extrabold uppercase">©{new Date().getFullYear()} - SpeedQuiz Team ❤️</footer>
---
+++ frontend/src/components/LoginCard.jsx
@@ -0,0 +1,7 @@
+const LoginCard = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
