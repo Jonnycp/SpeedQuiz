385d1d2146e6acacdc0bb788e687292acd8fa980
Jonnycp <jonathan-caputo@hotmail.com>
2026-08-04 23:13:01 +0200

    ConfirmButton ruotato
    Ricordiamoci della rotazione... fa brutti scherzi

--- frontend/src/components/ConfirmButton.jsx
+++ frontend/src/components/ConfirmButton.jsx
@@ -2,7 +2,7 @@ const ConfirmButton = ({ content, onClick }) => {
   return (
     <button 
       onClick={onClick}
-      className="w-full mt-4 bg-verdinoCarino border-3 border-neroNonNero shadow-buttons py-4 md:py-5 text-1xl md:text-4xl font-black uppercase text-black hover:scale-[1.02] active:scale-[0.98] transition-all duration-300 cursor-pointer"
+      className="w-full mt-4 -rotate-1 bg-verdinoCarino border-3 border-neroNonNero shadow-buttons py-4 md:py-5 text-1xl md:text-4xl font-black uppercase text-black hover:scale-[1.02] active:scale-[0.98] transition-all duration-300 cursor-pointer"
     >
       {content}
     </button>
