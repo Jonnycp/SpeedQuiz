a3c30e8a4b52eb8b6d25ca9ac40065ebf93356ff
Jonnycp <jonathan-caputo@hotmail.com>
2026-08-04 22:51:46 +0200

    Input impostazioni completate!

--- frontend/src/components/SettingsInput.jsx
+++ frontend/src/components/SettingsInput.jsx
@@ -1,28 +1,64 @@
+import { useState } from "react";
 import { Icon } from "@iconify/react";
+import RangeSlider from "./RangeSlider";
 
 const SettingsInput = ({
   label,
   name,
   type = "number",
+  minValue = 0,
+  maxValue = 10,
 }) => {
+  const [value, setValue] = useState(minValue);
+
   return (
-    <div className="flex flex-1 flex-col">
-      <label htmlFor={name}>{label}</label>
-      <div className="flex gap-2">
-        <button className="bg-white px-2 py-2 shadowbuttons border-3 border-neroNonNero">
-          <Icon icon="mdi:plus" />
-        </button>
-        <input
-          type={type}
-          id={name}
-          name={name}
-          min="1"
-          max="10"
-          defaultValue={3}
-        />
-        <button className="bg-white px-2 py-2 shadowbuttons border-3 border-neroNonNero">
-          <Icon icon="mdi:minus" />
-        </button>
+    <div className="flex flex-col basis-1/2">
+      <label className="whitespace-nowrap text-center select-none" htmlFor={name}>
+        {label}
+        {type === "range" && (
+          <span className="font-normal text-sm"> ({value} SEC)</span>
+        )}
+      </label>
+      <div className="flex w-full justify-between">
+        {type == "number" && (
+          <button
+            className="bg-white px-2 py-2 text-xl border-2 border-neroNonNero"
+            onClick={() => setValue(Math.max(value - 1, minValue))}
+          >
+            <Icon icon="mdi:minus" />
+          </button>
+        )}
+        {type == "range" ? (
+          <RangeSlider
+            min={minValue}
+            max={maxValue}
+            value={value}
+            onChange={setValue}
+          />
+        ) : (
+          <input
+            type={type}
+            id={name}
+            name={name}
+            min={minValue}
+            max={maxValue}
+            value={value}
+            onChange={(e) =>
+              setValue(Math.max(minValue, Math.min(maxValue, e.target.value)))
+            }
+            className="text-center w-fit text-2xl focus:outline-none 
+            [&::-webkit-inner-spin-button]:appearance-none"
+          />
+          /* apparence:none su webkit serve per eliminare la scrollbar dell'input inserita dal browser*/
+        )}
+        {type == "number" && (
+          <button
+            className="bg-white px-2 py-2 text-xl border-2 border-neroNonNero"
+            onClick={() => setValue(Math.min(value + 1, maxValue))}
+          >
+            <Icon icon="mdi:plus" />
+          </button>
+        )}
       </div>
     </div>
   );
--- frontend/src/pages/Lobby.jsx
+++ frontend/src/pages/Lobby.jsx
@@ -16,40 +16,43 @@ const Lobby = () => {
         username={username}
       />
       <div className="flex gap-20 mx-18 font-primary">
-        <aside className="bg-secondary flex flex-col items-center py-5 shadow-buttons flex-1 uppercase font-extrabold border-3 z-10 rounded-xl">
+        <aside className="bg-secondary flex flex-col items-center py-5 px-4 shadow-buttons flex-1 uppercase font-extrabold border-3 z-10 rounded-xl">
           <h3 className="bg-black text-white w-fit px-3 py-1 rounded-xl mb-4 select-none">
             codice stanza
           </h3>
-          <span className="block text-6xl tracking-wider">ABCD12</span>
+          <span className="block text-6xl tracking-wider ">ABCD12</span>
           <div className="flex gap-3 mt-5 mb-8 mx-auto select-none w-[80%]">
             <button className="bg-primary text-white grow px-5 py-2 uppercase font-bold shadow-buttons border-3 border-neroNonNero">
               Invita amici
             </button>
-        
+
             <button className="bg-white px-3 py-2 shadow-buttons border-3 border-neroNonNero">
-              <Icon icon="griddy-icons:copy" />
+              <Icon icon="tabler:copy" />
             </button>
           </div>
 
-          <form className="flex gap-10">
-            <SettingsInput label="Rounds" name="rounds"/>
-
-           
+          <form className="flex flex-col gap-5" onSubmit={(e) => e.preventDefault()}>
+            <div className="flex w-full gap-6">
+              <SettingsInput label="Rounds" name="rounds" minValue={3} maxValue={30} />
+              <SettingsInput label="Max Giocatori" name="maxPlayers" minValue={3} maxValue={8} />
+            </div>
+            <SettingsInput label="Tempo per rispondere" name="answerTime" type="range" minValue={10} maxValue={60} />
           </form>
         </aside>
 
         <section className="bg-white font-primary flex-2">
-            {/* Lista dei giocatori ma il primo è un host */}
-            {players.map((player, index) => (
-                <Gamer
-                    key={index}
-                    username={player}
-                    isHost={index === 0}
-                    imageUser={`https://api.dicebear.com/10.x/critters/svg?tags=animation&seed=${player}`}
-                    rotation={index % 2 === 0 ? "rotate-[0.3deg]" : "-rotate-[0.3deg]"}
-                />
-            ))}
-            <ConfirmButton content="Inizia partita"/>
+          {players.map((player, index) => (
+            <Gamer
+              key={index}
+              username={player}
+              isHost={index === 0}
+              imageUser={`https://api.dicebear.com/10.x/critters/svg?tags=animation&seed=${player}`}
+              rotation={
+                index % 2 === 0 ? "rotate-[0.3deg]" : "-rotate-[0.3deg]"
+              }
+            />
+          ))}
+          <ConfirmButton content="Inizia partita" />
         </section>
       </div>
     </>
