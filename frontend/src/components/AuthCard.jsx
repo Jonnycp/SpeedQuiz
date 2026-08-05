7da430a79f19d558a94ef071b5c2a8490aed3559
defeangelica <a.defeudis3@studenti.poliba.it>
2026-08-05 10:54:43 +0200

    add eye icon

--- frontend/src/components/AuthCard.jsx
+++ frontend/src/components/AuthCard.jsx
@@ -8,14 +8,16 @@ const AuthCard = ({isLogin=true}) => {
     const [showPassword, setPassword] = useState(false)    
 
     return (
-        <div className= "relative mx-auto font-primary w-fit">
-           <div className="absolute top left-4 z-10 bg-white border-3 border-neroNonNero shadow-buttons px-4 py-2">
-            <h1 className="text-terziary font-extrabold text-xl uppercase select-none">SPEEDQUIZ  </h1>
-           </div>
-
-           <form className="bg-white border-3 border-neroNonNero shadow-buttons px-6 py-10 flex flex-col gap-6">
+        
+       <div className="min-h-screen w-full flex flex-col items-center justify-center font-primary">
+            <div className="relative w-full max-w-md mx-auto">
+                <div className="absolute -top-6 left-4 z-10 bg-white border-3 border-neroNonNero shadow-buttons px-4 py-2">
+                   <h1 className="text-terziary font-extrabold text-xl uppercase select-none">SPEEDQUIZ</h1>
+            </div>
+       
+           <form className="w-full bg-white border-3 border-neroNonNero shadow-buttons px-10 py-10 flex flex-col gap-6">
             <div className="text-center">
-                <h1 className="font-extrabold text-3xl uppercase">
+                <h1 className="font-extrabold text-4xl uppercase ">
                     {isLogin? "Accedi" : "Crea un account"}
                 </h1>
                 <p className="text-black text-sm normal-case">Pronto a sfidare i tuoi amici?</p>
@@ -23,12 +25,12 @@ const AuthCard = ({isLogin=true}) => {
 
             {!isLogin && (
                 <div className="flex flex-col gap-1">
-                    <label htmlFor="username" className="font-bold uppercase text-block">Username</label>
+                    <label htmlFor="username" className="font-bold uppercase text-black ">Username</label>
                     <input 
                     type="text"
                     id="username"
                     placeholder="username"
-                    className="bg-white px-4 py-3 border-3 border-shadow border-neroNonNero"></input>
+                    className="bg-white px-4 py-3 border-3 shadow-buttons border-neroNonNero"></input>
                 </div>
             )}
 
@@ -38,26 +40,37 @@ const AuthCard = ({isLogin=true}) => {
                 type="text"
                 id="email"
                 placeholder="nome@esempio.it"
-                className="bg-white px-4 py-3 border-3 border-shadow border-neroNonNero placeholder:text-grey normal-case"></input>
+                className="bg-white px-4 py-3 border-3 border-shadow border-neroNonNero text-gray-400 normal-case shadow-buttons"></input>
             </div>
 
             <div className="flex flex-col gap-1">
-                <label htmlFor="password"
-                className="font-bold uppercase text-black">Password</label>
-                <input
-                type={showPassword ? "text" : "password"}
-                id="password"
-                placeholder="••••••••"
-                className="bg-white px-4 py-3 border-3 border-neroNonNero border-shadow">
-                </input>
+                <label htmlFor="password"className="font-bold uppercase text-black">Password</label>
+
+                <div className="relative">
+                    <input
+                    type={showPassword ? "text" : "password"}
+                    id="password"
+                    placeholder="••••••••"
+                    className="w-full bg-white px-4 py-3 border-3 border-neroNonNero shadow-buttons"
+                    />
+                    <button type="button"
+                    className="absolute top-4 right-3 cursor-pointer"
+                    onClick={() => setPassword(!showPassword)}> 
+                        <Icon icon={showPassword ? "mdi:eye-off" : "mdi-eye"} width={22}/>
+                    </button>
+                </div>
                 
             </div>
 
+            <button
+            type="submit"
+            className="bg-primary border-3 py-3 px-3 border-neroNonNero shadow-buttons cursor-pointer hover:scale-105 text-white uppercase font-bold">
+            {isLogin ? "Accedi" : "Registrati"}
+            </button>
 
            </form>
+               </div>
         </div>
-
-    
 
   )
 }
--- frontend/src/components/ConfirmButton.jsx
+++ frontend/src/components/ConfirmButton.jsx
@@ -1,8 +1,8 @@
-const ConfirmButton = ({ content, onClick }) => {
+const ConfirmButton = ({ content, onClick}) => {
   return (
     <button 
       onClick={onClick}
-      className="w-full mt-4 -rotate-1 bg-verdinoCarino border-3 border-neroNonNero shadow-buttons py-4 md:py-5 text-1xl md:text-4xl font-black uppercase text-black hover:scale-[1.02] active:scale-[0.98] transition-all duration-300 cursor-pointer"
+      className="bg-verdinoCarino w-full mt-4 -rotate-1 border-3 border-neroNonNero shadow-buttons py-4 md:py-5 text-1xl md:text-4xl font-black uppercase text-black hover:scale-[1.02] active:scale-[0.98] transition-all duration-300 cursor-pointer"
     >
       {content}
     </button>
