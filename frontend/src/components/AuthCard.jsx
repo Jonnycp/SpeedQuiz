5d55fb6265b9e298be94f25bb4a09c96f2923a45
defeangelica <a.defeudis3@studenti.poliba.it>
2026-08-05 15:16:15 +0200

    fix pass req, set shP

--- frontend/src/components/AuthCard.jsx
+++ frontend/src/components/AuthCard.jsx
@@ -3,12 +3,11 @@ import { Icon } from "@iconify/react";
 import { useState } from "react";
 
 
-const AuthCard = ({isLogin=true}) => {
-    const [showPassword, setPassword] = useState(false)    
+const AuthCard = ({isLogin}) => {
+    const [showPassword, setShowPassword] = useState(false)    
 
     return (
-        
-       <div className="min-h-screen w-full flex flex-col items-center justify-center font-primary">
+       <div className=" font-primary w-full flex flex-col my-10 md:my-20">
             <div className="relative w-full max-w-md mx-auto">
                 <div className="absolute -top-6 left-4 z-10 bg-white border-3 border-neroNonNero shadow-buttons px-4 py-2">
                    <h2 className="text-terziary font-extrabold text-xl uppercase select-none">SPEEDQUIZ</h2>
@@ -16,10 +15,10 @@ const AuthCard = ({isLogin=true}) => {
        
            <form className="w-full bg-white border-3 border-neroNonNero shadow-buttons px-10 py-10 flex flex-col gap-6">
             <div className="text-center">
-                <h1 className="font-extrabold text-4xl uppercase mt-3">
+                <h1 className="font-extrabold text-4xl uppercase mt-3 select-none">
                     {isLogin? "Accedi" : "Crea un account"}
                 </h1>
-                <p className="text-black text-sm normal-case">Pronto a sfidare i tuoi amici?</p>
+                <p className="text-black text-sm normal-case select-none">Pronto a sfidare i tuoi amici?</p>
             </div>
 
             {!isLogin && (
@@ -34,7 +33,7 @@ const AuthCard = ({isLogin=true}) => {
             )}
 
             <div className="flex flex-col gap-1">
-                <label htmlFor="email" className="font-bold uppercase text-black">Email</label>
+                <label htmlFor="email" className="font-bold uppercase text-black select-none">Email</label>
                 <input
                 type="text"
                 id="email"
@@ -43,22 +42,21 @@ const AuthCard = ({isLogin=true}) => {
             </div>
 
             <div className="flex flex-col gap-1">
-                <label htmlFor="password"className="font-bold uppercase text-black">Password</label>
+                <label htmlFor="password"className="font-bold uppercase text-black select-none">Password</label>
 
                 <div className="relative">
                     <input
                     type={showPassword ? "text" : "password"}
-                    id="password"
+                    id="password" required minLength={6}
                     placeholder="••••••••"
-                    className="w-full bg-white px-4 py-3 border-3 border-neroNonNero shadow-buttons"
-                    />
+                    className="w-full bg-white px-4 py-3 border-3 border-neroNonNero shadow-buttons"/>
+
                     <button type="button"
                     className="absolute top-4 right-3 cursor-pointer"
-                    onClick={() => setPassword(!showPassword)}> 
+                    onClick={() => setShowPassword(!showPassword)}> 
                         <Icon icon={showPassword ? "mdi:eye-off" : "mdi-eye"} width={22}/>
                     </button>
                 </div>
-                
             </div>
 
             <button
@@ -68,16 +66,13 @@ const AuthCard = ({isLogin=true}) => {
             </button>
             </form>
 
-
-            <p className="text-white text-center mt-6 text-sm normal-case"> 
+            <p className="text-white text-center mt-6 text-sm normal-case select-none"> 
                 {isLogin ? "Non hai un account?" : "Hai già un account?"}
-                <Link 
-                to={isLogin ? "/register" : "/login"} 
-                className="bg-secondary border-3 border-neroNonNero font-bold uppercase shadows-buttons text-black px-2 py-1 ml-2">
+                <Link to={isLogin ? "/register" : "/login"} 
+                className="bg-secondary border-3 border-neroNonNero font-bold uppercase shadow-buttons text-black px-2 py-1 ml-2">
                 {isLogin ? "Registrati" : "Accedi"}
                 </Link>
             </p>
-
         </div>
         </div>
   )
