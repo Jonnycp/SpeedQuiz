d4c7b6940e4f2baae468a621e298baefd381ee23
Jonnycp <jonathan-caputo@hotmail.com>
2026-08-06 16:04:59 +0200

    Immagine di sfondo

--- frontend/src/Game.jsx
+++ frontend/src/Game.jsx
@@ -1,13 +1,15 @@
 import { Icon } from "@iconify/react";
 import { BrowserRouter, Route, Routes } from "react-router";
 
+import background from "../assets/background.png";
+
 import Home from "./pages/Home.jsx";
 import Lobby from "./pages/Lobby.jsx";
 
 const Game = () => {
   return (
     <BrowserRouter>
-      <main>
+      <main style={{ backgroundImage: `url(${background})` }} className="bg-cover bg-no-repeat bg-center">
         <div className="absolute top-15 left- 5 md:top-32 md:left-10 text-9xl -rotate-12 select-none pointer-events-none"><Icon icon="noto:star"/></div>
         <div className="absolute top-160 right-5 md:top-130 md:right-10 text-9xl -rotate-12 select-none pointer-events-none"><Icon icon="noto:party-popper"/></div>
         <Routes>
