448ddb5d6b1736e346a84fc346a91e87989fa802
Luca <lugenba@gmail.com>
2026-08-06 12:47:27 +0200

    modifica su ConfirmButton per personalizzazione

--- frontend/src/components/ConfirmButton.jsx
+++ frontend/src/components/ConfirmButton.jsx
@@ -1,8 +1,11 @@
-const ConfirmButton = ({ content, onClick }) => {
+const ConfirmButton = ({ content, onClick, customClasses }) => {
+  const defaultClasses = "w-full mt-4 -rotate-1 bg-verdinoCarino py-4 md:py-5 text-xl md:text-4xl";
+
+  const classesToUse = customClasses || defaultClasses;
   return (
     <button 
       onClick={onClick}
-      className="w-full mt-4 -rotate-1 bg-verdinoCarino border-3 border-neroNonNero shadow-buttons py-4 md:py-5 text-1xl md:text-4xl font-black uppercase text-black hover:scale-[1.02] active:scale-[0.98] transition-all duration-300 cursor-pointer"
+      className={`border-3 border-neroNonNero shadow-buttons font-black uppercase text-black hover:scale-[1.02] active:scale-[0.98] transition-all duration-300 cursor-pointer ${classesToUse}`}
     >
       {content}
     </button>
