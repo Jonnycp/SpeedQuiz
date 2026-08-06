fd56bb244e5d5038987648110c3dc7da8c9d8c68
defeangelica <a.defeudis3@studenti.poliba.it>
2026-08-06 19:16:24 +0200

    v2def quest-card

--- frontend/src/components/QuestionCard.jsx
+++ frontend/src/components/QuestionCard.jsx
@@ -7,8 +7,8 @@ import GamePhase from "./GamePhase"
 const Question = () => {
 
     return(
-        <div className="flex flex-col items-center mt-30 md:mt-30 px-4 gap-6 w-full max-w-2xl mx-auto"> 
-            <div className="relative w-full bg-white shadow-buttons border-3 border-neroNonNero px-6 py-6 md:px-17 md:py-8 rounded-2xl select-none">
+        <div className="flex flex-col items-center mt-30 md:mt-30 px-4 gap-6 w-full mx-auto md:max-w-3xl"> 
+            <div className="relative w-full bg-white shadow-buttons border-3 border-neroNonNero px-6 py-7 md:px-20 md:py-10 rounded-2xl select-none">
                 <h2 className="text-2xl md:text-3xl font-extrabold text-center">Tre cose che vorresti dire a Ferrara </h2>
                 <p className="absolute uppercase -top-3 md:-top-5 -left-4 bg-[#FFDF9A] shadow-buttons text-xs px-3 py-1 font-bold -rotate-2 select-none md:text-base">domanda flash</p>
                 <p className="absolute uppercase -bottom-3 -right-2 bg-[#C4C0FF] shadow-buttons text-xs px-3 py-1 rotate-3 text-[#2100A4] font-bold select-none md:text-base">non mentire!</p>
@@ -17,9 +17,9 @@ const Question = () => {
             <form className="w-[80%]">
 
                 <div className="flex flex-col gap-5 *:w-full *:px-4 md:*:px-6 *:py-3 md:*:py-4 *:text-sm md:*:text-lg *:border-3 *:border-neroNonNero *:shadow-buttons mt-7 *:placeholder:text-gray-400">
-                    <input placeholder="Inserisci la risposta..." className="bg-white py-3 -rotate-[1.2deg]"></input>
-                    <input placeholder="Inserisci la risposta..." className="bg-white py-3 rotate-1"></input>
-                    <input placeholder="Inserisci la risposta..." className="bg-white py-3 md:z-10"></input>
+                    <input placeholder="Inserisci la risposta..." className="bg-white -rotate-[1.2deg]"></input>
+                    <input placeholder="Inserisci la risposta..." className="bg-white rotate-1"></input>
+                    <input placeholder="Inserisci la risposta..." className="bg-white md:z-10"></input>
                 </div>
 
                 <div className="w-1/2 mx-auto mt-15">
