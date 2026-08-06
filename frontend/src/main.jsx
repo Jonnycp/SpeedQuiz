4c821fd71ddd2a0b2bbbd89e452b315c08e5e3ac
defeangelica <a.defeudis3@studenti.poliba.it>
2026-08-06 16:56:44 +0200

    fix confirm-button

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 import Game from './Game.jsx'
-import QuestionCard from './components/QuestionCard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <QuestionCard />
+    <Game />
   </StrictMode>,
 )
-
-// Game settalo al posto di Question
-// rimuovi import question
