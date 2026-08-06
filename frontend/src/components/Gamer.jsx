0395769b68e102b3bb985337eb82d6fb0ab92bdb
Jonnycp <jonathan-caputo@hotmail.com>
2026-08-06 15:38:18 +0200

    Rimosso prop immagine utente.. poi vedremo

--- frontend/src/components/Gamer.jsx
+++ frontend/src/components/Gamer.jsx
@@ -1,10 +1,10 @@
-const Gamer = ({username, imageUser, isHost = false, rotation}) => {
+const Gamer = ({username, isHost = false, rotation}) => {
   return (
-<div className={`flex flex-col md:flex-row items-center md:justify-between md:w-full md:max-w-2xl md:bg-white md:border-3 md:border-neroNonNero md:shadow-buttons md:py-3 md:px-5 font-primary md:mx-auto ${rotation}`}>
+<div className={`flex flex-col md:flex-row items-center md:justify-between md:w-full md:max-w-2xl md:bg-white md:border-3 md:border-neroNonNero md:shadow-buttons md:py-3 md:px-5 font-primary md:mx-auto ${rotation || ""}`}>
         <div className="relative flex flex-col items-center md:flex-row md:gap-4 w-18 md:w-full">
             <img
                 className="w-16 md:w-8 md:h-8 rounded-full border-3 border-neroNonNero shadow-buttons md:shadow-none"
-                src={imageUser}
+                src={`https://api.dicebear.com/10.x/critters/svg?tags=animation&seed=${username}`}
                 alt={`Avatar di ${username}`}
             />
             <span className="font-bold text-xs md:text-xl mt-3 md:mt-0 uppercase tracking-wide truncate max-w-full text-white md:text-black">
