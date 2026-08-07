d0570a5979cab3c9c0389b25f0059b1f24cd5429
Jonnycp <jonathan-caputo@hotmail.com>
2026-08-07 10:57:40 +0200

    Background image funzionante

--- frontend/src/Game.jsx
+++ frontend/src/Game.jsx
@@ -9,18 +9,27 @@ import Vote from "./pages/Vote.jsx";
 
 const Game = () => {
   return (
-    <BrowserRouter>
-      <main style={{ backgroundImage: `url(${background})` }} className="bg-cover bg-no-repeat bg-center">
-        <div className="absolute top-15 left- 5 md:top-32 md:left-10 text-9xl -rotate-12 select-none pointer-events-none"><Icon icon="noto:star"/></div>
-        <div className="absolute top-160 right-5 md:top-130 md:right-10 text-9xl -rotate-12 select-none pointer-events-none"><Icon icon="noto:party-popper"/></div>
+    <main
+      style={{ backgroundImage: `url(${background})` }}
+      className="bg-repeat bg-center"
+    >
+      <BrowserRouter>
+        <div className="absolute top-15 left- 5 md:top-32 md:left-10 text-9xl -rotate-12 select-none pointer-events-none">
+          <Icon icon="noto:star" />
+        </div>
+        <div className="absolute top-160 right-5 md:top-130 md:right-10 text-9xl -rotate-12 select-none pointer-events-none">
+          <Icon icon="noto:party-popper" />
+        </div>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/lobby" element={<Lobby />} />
           <Route path="/vote" element={<Vote />} />
         </Routes>
-      </main>
-      <footer className="text-white/50 font-primary text-sm text-center my-5 font-extrabold uppercase selection:bg-primary selection:text-black">©{new Date().getFullYear()} - SpeedQuiz Team ❤️</footer>
-    </BrowserRouter>
+        <footer className="text-white/50 font-primary text-sm text-center py-5 font-extrabold uppercase selection:bg-primary selection:text-black">
+          ©{new Date().getFullYear()} - SpeedQuiz Team ❤️
+        </footer>
+      </BrowserRouter>
+    </main>
   );
 };
 
