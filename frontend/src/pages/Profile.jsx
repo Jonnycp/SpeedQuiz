91d03e2d7f14c764f2021e5d56c931f90ad1a0b8
Luca <lugenba@gmail.com>
2026-08-07 14:25:42 +0200

    struttura di Profile
    modificare il form, la prima section e sistemare la struttura di pagina

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,6 +1,8 @@
 import Header from "../components/Header";
+import LobbyCard from "../components/LobbyCard";
 import SectionTitle from "../components/SectionTitle";
 import StatCardProfile from "../components/StatCardProfile";
+import { Icon } from "@iconify/react";
 
 const Profile = () => {
     const punti = 7083;
@@ -26,9 +28,54 @@ const Profile = () => {
         <div>
             <SectionTitle title="Le tue partite" />
         </div>
-        <div>
-            <SectionTitle title="Il tuo profilo" />
+
+        <section className="w-full max-w-5xl grid grid-cols-1 md:grid-cols-2 gap-12">
+        {/* sx*/}
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+            <LobbyCard title="Stanza di tizio 1" players={3} players_max={3} rotation="-rotate-1" content="Rivedi" />
+            <LobbyCard title="Stanza di tizio 1" players={3} players_max={10} rotation="-rotate-1" content="Rivedi" />
+            <LobbyCard title="Stanza di tizio 1" players={3} players_max={6} rotation="-rotate-1" content="Rivedi" />
+            <LobbyCard title="Stanza di tizio 1" players={3} players_max={4} rotation="-rotate-1" content="Rivedi" />
         </div>
+        {/* dx*/}
+        <div>
+          <SectionTitle title="IL TUO PROFILO" />     
+          <div className="bg-white border-4 border-neroNonNero shadow-[12px_12px_0_0_#000] p-6 md:p-8 rotate-[0.5deg]">
+            <form className="flex flex-col gap-2">
+                <label htmlFor="username" className="font-extrabold text-lg md:text-xl uppercase text-black">Username</label>
+                <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    placeholder="Inserisci il tuo username"
+                    className="bg-gray-200 p-2 md:p-3 border-3 border-neroNonNero placeholder:text-gray-400 placeholder:text-lg placeholder:font-medium focus:outline-none"
+                />
+                <label htmlFor="email" className="font-extrabold text-lg md:text-xl uppercase text-black">Email</label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Inserisci la tua email"
+                    className="bg-gray-200 p-2 md:p-3 border-3 border-neroNonNero placeholder:text-gray-400 placeholder:text-lg placeholder:font-medium focus:outline-none"
+                />
+                <label htmlFor="password" className="font-extrabold text-lg md:text-xl uppercase text-black">Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="Inserisci la tua password"
+                    className="bg-gray-200 p-2 md:p-3 border-3 border-neroNonNero placeholder:text-gray-400 placeholder:text-lg placeholder:font-medium focus:outline-none"
+                />
+            </form>
+              <button 
+                type="button" 
+                className="w-full mt-6 bg-verdinoCarino text-white border-3 border-neroNonNero py-4 font-black uppercase text-base md:text-lg shadow-[6px_6px_0_0_#000] hover:-translate-y-1 transition-transform flex justify-center items-center gap-2 cursor-pointer"
+              >
+                SALVA MODIFICHE <Icon icon="mdi:send" className="text-xl"/>
+              </button>
+          </div>
+        </div>
+      </section>
     </>
     )
 }
