79d9d2fb370f058f3563b43ecbd5d40f960fc616
Jonnycp <jonathan-caputo@hotmail.com>
2026-08-07 12:36:28 +0200

    Implementato mainTitle

--- frontend/src/pages/Leaderboard.jsx
+++ frontend/src/pages/Leaderboard.jsx
@@ -5,18 +5,19 @@ import Header from "../components/Header";
 import ResultPlayer from "../components/ResultPlayer";
 import PodiumPlayer from "../components/PodiumPlayer";
 import ConfirmButton from "../components/ConfirmButton";
+import MainTitle from "../components/MainTitle";
 
 const Leaderboard = () => {
   return (
     <>
       <Header username="Jonathan" />
       <Confetti />
-      <div>
-        <h1 className="text-4xl text-white font-primary uppercase">
-          Partita conclusa
-        </h1>
-        Abbiamo solo 3 vincitori! Fagli un applauso!
-      </div>
+      <section className="my-10">
+        <MainTitle title="Partita conclusa!" className="text-5xl"/>
+        <h2 className="text-xl text-semibold font-primary text-center text-secondary mt-4">
+          3 finalisti, nessuna dignità: applausi
+        </h2>
+      </section>
       <section className="flex gap-3 md:gap-8 mx-4 md:mx-32 py-24 relative z-10 select-none">
         <PodiumPlayer position={2} username="Maria" points={800} />
         <PodiumPlayer position={1} username="Antonio" points={1000} />
--- frontend/src/pages/Question.jsx
+++ frontend/src/pages/Question.jsx
@@ -1,15 +1,20 @@
-import GamePhase from '../components/GamePhase.jsx'
-import QuestionCard from '../components/QuestionCard.jsx'
+import GamePhase from "../components/GamePhase.jsx";
+import QuestionCard from "../components/QuestionCard.jsx";
+import MainTitle from "../components/MainTitle.jsx";
+import TimeSlider from "../components/TimeSlider.jsx";
 
 const Question = () => {
+  return (
+    <>
+      <GamePhase underPhase="Round 1 di 3" username="Jonathan" />
+      <section className="flex flex-col items-center justify-center relative z-10 md:mt-10 mt-5">
+        <MainTitle title="Domanda 1 di 3" />
+        <TimeSlider tempoIniziale={20} />
+      </section>
 
-    return(
-        <>
-        <GamePhase underPhase="Round 1 di 3" username="Jonathan" />
-        <QuestionCard/>
-        </>
-
-    )
-}
+      <QuestionCard />
+    </>
+  );
+};
 
 export default Question;
