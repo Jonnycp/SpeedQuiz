4e346c2296245b408c63b272210513ab6279a9c2
Jonnycp <jonathan-caputo@hotmail.com>
2026-08-07 11:44:13 +0200

    Aggiunto maxLength
    di nuovo.. si
    branch sbagliato

--- frontend/src/components/QuestionCard.jsx
+++ frontend/src/components/QuestionCard.jsx
@@ -17,12 +17,12 @@ const Question = () => {
             <form className="w-[80%]">
 
                 <div className="flex flex-col gap-5 *:w-full *:px-4 md:*:px-6 *:py-3 md:*:py-4 *:text-sm md:*:text-lg *:border-3 *:border-neroNonNero *:shadow-buttons mt-7 *:placeholder:text-gray-400">
-                    <input placeholder="Inserisci la risposta..." className="bg-white -rotate-[1.2deg]"></input>
-                    <input placeholder="Inserisci la risposta..." className="bg-white rotate-1"></input>
-                    <input placeholder="Inserisci la risposta..." className="bg-white md:z-10"></input>
+                    <input placeholder="Inserisci la risposta..." maxLength={50} className="bg-white rotate-[-1.2deg]"></input>
+                    <input placeholder="Inserisci la risposta..." maxLength={50} className="bg-white rotate-1"></input>
+                    <input placeholder="Inserisci la risposta..." maxLength={50} className="bg-white md:z-10"></input>
                 </div>
 
-                <div className="w-1/2 mx-auto mt-15">
+                <div className="w-1/2 mx-auto my-15">
                     <ConfirmButton content="Invia" customClasses="w-full bg-primary text-white -rotate-1 py-4 md:py-3 text-3xl md:text-4xl"/>
                 </div>
 
