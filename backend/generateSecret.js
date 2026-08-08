52194cb377a45c5f03eec06759e340d3042855ac
Jonnycp <jonathan-caputo@hotmail.com>
2026-08-08 19:16:56 +0200

    🥲Siamo tornati a commonjs
    Nostalgia... dimenticata
    
    Co-Authored-By: defeangelica <234227725+defeangelica@users.noreply.github.com>

--- backend/generateSecret.js
+++ backend/generateSecret.js
@@ -1,4 +1,4 @@
-import crypto from "crypto"
+const crypto = require("crypto")
 
 console.log("\n--------\nTieni al sicuro questa chiave nel .env")
 console.log(crypto.randomBytes(32).toString("hex")+"\n--------\n\n")
