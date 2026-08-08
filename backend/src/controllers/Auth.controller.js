50cbfda44c0ea4d2b0c9e9ff3f4c0bec885ee7d0
Jonnycp <jonathan-caputo@hotmail.com>
2026-08-08 17:58:40 +0200

    Registrazione funzionante con JWT
    Topperia!
    
    Co-Authored-By: defeangelica <234227725+defeangelica@users.noreply.github.com>

--- backend/src/controllers/Auth.controller.js
+++ backend/src/controllers/Auth.controller.js
@@ -1,6 +1,18 @@
 const User = require("../models/User.js");
 const jwt = require("jsonwebtoken");
 
+const EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+
+function generateJWT(userId){
+  const token = jwt.sign({
+        userId: userId,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+    );
+  return token;
+}
+
 /**
  * Enpoint POST /login
  * Effettua il login creando un JWT token
@@ -8,7 +20,7 @@ const jwt = require("jsonwebtoken");
  */
 async function login(req, res) {
   try {
-    if (!req.body || !req.body.email || !req.body.password) {
+    if (!req.body || !req.body.email.trim() || !req.body.password.trim()) {
       return res.status(400).json({ message: "Email e password sono obbigatori." });
     }
     const email = req.body.email.trim().toLowerCase();
@@ -25,24 +37,71 @@ async function login(req, res) {
       return res.status(401).json({ message: "Email o password non valide" });
     }
 
-    // Genera access token JWT
-    const token = jwt.sign({
-        userId: user._id,
-      },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" },
-    );
-
     return res.status(200).json({
-        token,
-        user: { id: user._id, username: user.username, email: user.email },
+        token: generateJWT(user._id),
+        user: { 
+          id: user._id, 
+          username: user.username, 
+          email: user.email },
       });
 
   } catch (err) {
-    return res.status(500).json({ message: "Impossibile effettuare il login ora..." });
+    return res.status(500).json({ message: "Impossibile effettuare il login ora." });
+  }
+}
+
+/**
+ * Enpoint POST /register
+ * Effettua la registrazione di un nuovo utente
+ * Parametri body: email, password, username
+ */
+async function register(req, res) {
+  try {
+
+    if (!req.body || !req.body.email.trim() || !req.body.password.trim() || !req.body.username.trim()) {
+      return res.status(400).json({ message: "Email, password e username sono obbigatori." });
+    }
+
+    const email = req.body.email.trim().toLowerCase();
+    const password = req.body.password.trim();
+    const username = req.body.username.trim().toLowerCase();
+
+    if(password.length < 8){
+      return res.status(400).json({message: "Scegli una password più sicura."})
+    }
+    if(!EMAIL_REGEX.test(email)){
+      return res.status(400).json({message: "Email non valida."})
+    }
+    if(username.length < 2 || username.length > 15){
+      return res.status(400).json({message: "Username non valido."})
+    }
+    
+    const existingEmail = await User.findOne({email});
+    const existingUsername = await User.findOne({username});
+    
+    if(existingEmail){
+      return res.status(400).json({message: "Email già registrata. Effettua il login."});
+    } else if(existingUsername){
+      return res.status(400).json({message: "Username già esistente. Prova con un altro ;)"});
+    }
+    
+    const newUser = new User({username, email, password});
+    await newUser.save();
+    
+    return res.status(201).json({
+      token: generateJWT(newUser._id),
+      user: { 
+        id: newUser._id, 
+        username: newUser.username, 
+        email: newUser.email },
+    });
+  } catch (err){
+    console.error(err)
+    return res.status(500).json({ message: "Impossibile effettuare la registrazione ora." });
   }
 }
 
 module.exports = {
   login,
+  register,
 };
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -5,4 +5,7 @@ const router = express.Router();
 
 router.post("/login", AuthController.login);
 
+router.post("/register", AuthController.register);
+
+
 module.exports = router;
