5b77083c203a78de9c6ae0b02a01df98e551e498
Jonnycp <jonathan-caputo@hotmail.com>
2026-08-09 14:02:20 +0200

    Aggiunto AuthProvider
    Co-Authored-By: defeangelica <234227725+defeangelica@users.noreply.github.com>

--- frontend/src/Game.jsx
+++ frontend/src/Game.jsx
@@ -1,5 +1,6 @@
 import { Icon } from "@iconify/react";
 import { BrowserRouter, Route, Routes } from "react-router";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
 
 import background from "../assets/background.png";
 
@@ -24,15 +25,17 @@ const Game = () => {
         <div className="absolute top-160 right-5 md:top-130 md:right-10 text-9xl -rotate-12 select-none pointer-events-none">
           <Icon icon="noto:party-popper" />
         </div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/lobby" element={<Lobby />} />
-          <Route path="/vote" element={<Vote />} />
-          <Route path="/question" element={<Question />} />
-          <Route path="/leaderboard" element={<Leaderboard />} />
-        </Routes>
+        <AuthProvider>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/lobby" element={<Lobby />} />
+              <Route path="/vote" element={<Vote />} />
+              <Route path="/question" element={<Question />} />
+              <Route path="/leaderboard" element={<Leaderboard />} />
+            </Routes>
+        </AuthProvider>
         <footer className="text-white/50 font-primary text-sm text-center py-5 font-extrabold uppercase selection:bg-primary selection:text-black">
           ©{new Date().getFullYear()} - SpeedQuiz Team ❤️
         </footer>
