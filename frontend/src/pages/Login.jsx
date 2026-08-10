2ff5daa66a871a0fbd274fc398fe7e9ec2f65670
Jonnycp <jonathan-caputo@hotmail.com>
2026-08-10 13:15:21 +0200

    Risolto flashing pulsante Confirm
    Bastava cambiare isLoading di default

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
 
   const { login } = useAuth();
   const navigate = useNavigate();
