9f28c4cb6a15625bbc507fda294b914dc419d5cf
Jonnycp <jonathan-caputo@hotmail.com>
2026-08-10 12:38:02 +0200

    😁 Login e registrazione funzionante
    No way... soddisfazioni
    
    Co-Authored-By: Luca <92517296+GentileLuca@users.noreply.github.com>
    Co-Authored-By: defeangelica <234227725+defeangelica@users.noreply.github.com>

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,33 +1,47 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
 import { useAuth } from "../contexts/AuthContext";
 
 import AuthCard from "../components/AuthCard";
 import SingleInput from "../components/SingleInput";
 
+const EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false)
 
   const { login } = useAuth();
   const navigate = useNavigate();
-  
+
+  useEffect(() => {
+    if(password.length < 8 || !EMAIL_REGEX.test(email)){
+      setIsLoading(true)
+    }else{
+      setIsLoading(false)
+    }
+  }, [email, password])
+
   async function handleSubmit(e){
     e.preventDefault();
     setError("");
-
+    setIsLoading(true)
     try{
       await login(email, password);
       navigate("/");
     }catch(err) {
-      setError(err || "Al momento abbiamo qualche problema ad eseguire il login :(");
+      setError(err || {message: "Al momento abbiamo qualche problema ad eseguire il login :("});
+    }finally{
+      setIsLoading(false)
     }
   }
 
   return (
-    <AuthCard isLogin={true} onSubmit={handleSubmit}>
-        <SingleInput
+    <AuthCard isLogin={true} onSubmit={handleSubmit} isLoading={isLoading}>
+      {error && <p className="text-red-500 text-center text-md mb-2">{error.message}</p>}
+      <SingleInput
           label="Email"
           type="email"
           name="email"
@@ -42,7 +56,7 @@ const Login = () => {
           value={password}
           minLength={8}
           onChange={(e) => setPassword(e.target.value)}
-          placeholder="••••••••"
+          placeholder="• • • • • • • •"
         />
     </AuthCard>
   );
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,15 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AuthCard from "../components/AuthCard";
 import SingleInput from "../components/SingleInput";
+import { useAuth } from "../contexts/AuthContext";
+import { useNavigate } from "react-router";
+
+const EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
 
 const Register = () => {
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+    const { register } = useAuth();
+    const navigate = useNavigate();
+
+    async function handleSubmit(e){
+        e.preventDefault();
+        setError("");
+        setIsLoading(true);
+        try{
+            await register(username, email, password);
+            navigate("/");
+        }catch(error){
+          setError(error || {message: "Al momento abbiamo qualche problema ad eseguire la registrazione :("});
+        }finally{
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        if(username.length < 2 || username.length>15 || !EMAIL_REGEX.test(email) || password.length<8){
+          setIsLoading(true);
+        }else{
+          setIsLoading(false);
+        }
+    }, [username, email, password])
     
     return (
-    <AuthCard isLogin={false}>
-      <form>
+    <AuthCard isLogin={false} onSubmit={handleSubmit} isLoading={isLoading}>
+        {error && <p className="text-red-500 text-center text-md mb-2">{error.essage}</p>}
          <SingleInput
           label="Username"
           type="text"
@@ -17,6 +48,8 @@ const Register = () => {
           value={username}
           onChange={(e) => setUsername(e.target.value)}
           placeholder="Nome di gioco"
+          minLength={2}
+          maxLength={15}
         />
         <SingleInput
           label="Email"
@@ -33,9 +66,8 @@ const Register = () => {
           value={password}
           minLength={8}
           onChange={(e) => setPassword(e.target.value)}
-          placeholder="••••••••"
+          placeholder="• • • • • • • •"
         />
-      </form>
     </AuthCard>
     )
 }
