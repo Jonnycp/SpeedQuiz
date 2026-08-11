4999034b3a61b6702e205993b1d5dfff6a746328
Jonathan <jonathan-caputo@hotmail.com>
2026-08-11 22:34:05 +0200

    😩 Il player ora può joinare una lobby
    In teoria, dovrebbe funzionare... ora lo testiamo... speriamo bene
    Siamo abbastanza stanchi
    
    Co-Authored-By: defeangelica <234227725+defeangelica@users.noreply.github.com>

--- backend/src/services/lobby.service.js
+++ backend/src/services/lobby.service.js
@@ -23,10 +23,48 @@ function createLobby(ownerId) {
     return newLobby
 }
 
-function addPlayer(){
+function addPlayer(lobby, socket){
+    //* Gestione persone che erano entrate e si sono disconnesse per sbaglio
+    const existingPlayer = lobby.player.get(socket.user.id)
+    
+    //* Attaccante che è connesso e chiama dinuovo la funzione
+    if(existingPlayer && existingPlayer.connected){
+        throw new Error("Sei già in questa stanza!")
+    }
 
+    //* Fai rientrare utente disconnesso
+    if(existingPlayer && !existingPlayer.connected){
+        existingPlayer.socketId = socket.id;
+        existingPlayer.connected = true;
+        existingPlayer.disconnectedAt = null;
+
+        return existingPlayer
+    }
+
+    //* Controlli per fare entrare nuovo giocatore
+    if(lobby.players.size >= lobby.config.maxPlayers){
+        throw new Error("La stanza è piena")
+    }
+
+    if(lobby.status !== "LOBBY"){
+        throw new Error("Partita già iniziata")
+    }
+
+    //* Nuovo giocatore dentro
+    const newPlayer = {
+        id: socket.user.id,
+        username: socket.user.username,
+        socketId: socket.id,
+        connected: true,
+        disconnectedAt: null,
+        score: 0
+    }
+
+    lobby.players.set(newPlayer.id, newPlayer)
+    return newPlayer
 }
 
 module.exports = {
-    createLobby
+    createLobby,
+    addPlayer
 }
