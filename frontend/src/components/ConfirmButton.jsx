972c905d39a09f8ff680e0d43f5dcca5e042bc94
Luca <lugenba@gmail.com>
2026-08-13 12:05:46 +0200

    modificato il confirmButton

--- frontend/src/components/ConfirmButton.jsx
+++ frontend/src/components/ConfirmButton.jsx
@@ -1,19 +1,29 @@
-const ConfirmButton = ({ content, onClick, type, disabled, customClasses, bgColor, textColor}) => {
+const ConfirmButton = ({ children, onClick, type, disabled, customClasses, color}) => {
+
+  const colorVariants = {
+    primary: "bg-primary text-white",
+    secondary: "bg-secondary text-black",
+    verdinoCarino: "bg-verdinoCarino text-black",
+    terziary: "bg-terziary text-white",
+    red: "bg-red-600 text-white",
+    gray: "bg-gray-300 text-black"
+  };
+
   return (
     <button 
       type={type}
       onClick={onClick}
       disabled={disabled}
       className={`
-        text-${textColor} font-black px-6 py-2 uppercase border-3 border-neroNonNero transition-all duration-300 
+        font-black px-6 py-2 uppercase border-3 border-neroNonNero transition-all duration-300 
         ${disabled 
-          ? "bg-gray-400 opacity-80 cursor-not-allowed shadow-[4px_4px_0_0_#000]" 
-          : `bg-${bgColor} shadow-buttons cursor-pointer hover:bg-${bgColor}-700 hover:scale-95 active:translate-y-1 active:translate-x-1 active:shadow-none`
+          ? "bg-gray-400 text-white opacity-80 cursor-not-allowed shadow-buttons" 
+          : `${colorVariants[color]} shadow-buttons cursor-pointer hover:scale-95 hover:brightness-95 active:translate-y-1 active:translate-x-1 active:shadow-none`
         } 
-        ${customClasses}
+        ${customClasses || ""}
       `}
     >
-      {content}
+      {children}
     </button>
   );
 };
