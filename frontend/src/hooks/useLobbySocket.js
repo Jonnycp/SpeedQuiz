0539014524b2551a564656c6d5a878955408d89a
Jonathan <jonathan-caputo@hotmail.com>
2026-08-13 19:37:56 +0200

    Hook personalizzato per LISTENERs Lobby
    Raggruppati qui... ma forse non è il miglior modo.
    Ci abbiamo provato a trovare soluzioni più efficienti, ma sembra essere top
    
    Co-Authored-By: defeangelica <234227725+defeangelica@users.noreply.github.com>

---
+++ frontend/src/hooks/useLobbySocket.js
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+
+/* 1. capire se divisione in hook è giusta
+2. capire dove è il miglior posto per inserire emittori
+3. capire come usare ack
+4. far funzionare tutto...entro oggi :( ) */
+
+export function useLobbySocket(socket, setLobby) {
+  //* ASCOLTATORI
+  useEffect(() => {
+    if (!socket) return;
+
+    socket.on("lobby:joined", (data) => {
+      console.log("lobby:joined", data);
+      setLobby(data.lobby);
+    });
+
+    socket.on("lobby:error", (data) => {
+      console.log("lobby:error", data);
+    });
+
+    socket.on("lobby:player_joined", (data) => {
+      console.log("lobby:player_joined", data);
+      setLobby(data.lobby);
+    });
+
+    socket.on("lobby:player_disconnected", (data) => {
+      console.log("lobby:player_disconnected", data);
+      setLobby(data.lobby);
+    });
+
+    socket.on("lobby:player_offline", (data) => {
+      console.log("lobby:player_offline", data);
+      setLobby(data.lobby);
+    });
+
+    return () => {
+      socket.off("lobby:joined");
+      socket.off("lobby:player_joined");
+      socket.off("lobby:player_offline");
+      socket.off("lobby:error");
+      socket.off("lobby:player_disconnected");
+    };
+  }, [socket, setLobby]);
+}
