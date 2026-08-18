fbc3cfe1e1d3a5af6dd0b8d4562a39326b800202
Jonathan <jonathan-caputo@hotmail.com>
2026-08-18 17:38:06 +0200

    Iniziata questionCard dinamica
    Iniziamo iterando per i vari input
    
    Co-Authored-By: defeangelica <234227725+defeangelica@users.noreply.github.com>

--- frontend/src/components/QuestionCard.jsx
+++ frontend/src/components/QuestionCard.jsx
@@ -1,32 +1,62 @@
-import ConfirmButton from "./ConfirmButton"
-import GamePhase from "./GamePhase"
+import { useState } from "react";
+import ConfirmButton from "./ConfirmButton";
 
+const QuestionCard = ({ question, onSubmit }) => {
+  const [answers, setAnswers] = useState(["", "", ""]);
 
-const QuestionCard = () => {
+  function handleChange(index, value) {
+    setAnswers(answers.map((answer, i) => (i === index ? value : answer)));
+  }
 
-    return(
-        <div className="flex flex-col items-center mt-5 px-4 gap-3 w-full mx-auto md:max-w-3xl"> 
-            <div className="relative w-full bg-white shadow-buttons border-3 border-neroNonNero px-6 py-7 md:px-20 md:py-10 rounded-2xl select-none">
-                <h2 className="text-2xl md:text-3xl font-extrabold text-center">Tre cose che vorresti dire a Ferrara </h2>
-                <p className="absolute uppercase -top-3 md:-top-5 -left-4 bg-[#FFDF9A] shadow-buttons text-xs px-3 py-1 font-bold -rotate-2 select-none md:text-base">domanda flash</p>
-                <p className="absolute uppercase -bottom-3 -right-2 bg-[#C4C0FF] shadow-buttons text-xs px-3 py-1 rotate-3 text-[#2100A4] font-bold select-none md:text-base">non mentire!</p>
-            </div>
+  
+  return (
+    <div className="flex flex-col items-center mt-5 px-4 gap-3 w-full mx-auto md:max-w-3xl">
+      <div className="relative w-full bg-white shadow-buttons border-3 border-neroNonNero px-6 py-7 md:px-20 md:py-10 rounded-2xl select-none">
+        <h2 className="text-2xl md:text-3xl font-extrabold text-center">
+          {question.text}
+        </h2>
+        <p className="absolute uppercase -top-3 md:-top-5 -left-4 bg-[#FFDF9A] shadow-buttons text-xs px-3 py-1 font-bold -rotate-2 select-none md:text-base">
+          domanda flash
+        </p>
+        <p className="absolute uppercase -bottom-3 -right-2 bg-[#C4C0FF] shadow-buttons text-xs px-3 py-1 rotate-3 text-[#2100A4] font-bold select-none md:text-base">
+          non mentire!
+        </p>
+      </div>
 
-            <form className="w-[80%]">
-                <div className="flex flex-col gap-5 *:w-full *:px-4 md:*:px-6 *:py-3 md:*:py-4 *:text-sm md:*:text-lg *:border-3 *:border-neroNonNero *:shadow-buttons mt-7 *:placeholder:text-gray-400">
-                    <input placeholder="Inserisci la risposta..." maxLength={50} className="bg-white rotate-[-1.2deg]"></input>
-                    <input placeholder="Inserisci la risposta..." maxLength={50} className="bg-white rotate-1"></input>
-                    <input placeholder="Inserisci la risposta..." maxLength={50} className="bg-white md:z-10"></input>
-                </div>
-
-                <div className="w-1/2 mx-auto mt-10 mb-5">
-                    <ConfirmButton color="primary" type="submit" customClasses="w-full -rotate-1 py-4 md:py-3 text-3xl md:text-4xl">
-                        Invia
-                    </ConfirmButton>
-                </div>
-
-            </form>
+      <form
+        className="w-[80%]"
+        onSubmit={(e) => {
+          e.preventDefault();
+          onSubmit(answers);
+        }}
+      >
+        <div className="flex flex-col gap-5 *:w-full *:px-4 md:*:px-6 *:py-4 *:text-sm md:*:text-lg *:border-3 *:border-neroNonNero *:shadow-buttons mt-7 *:placeholder:text-gray-400">
+          {answers.map((value, i) => {
+            return (
+              <input
+                key={i}
+                value={value}
+                placeholder="Inserisci la risposta..."
+                onChange={(e) => handleChange(i, e.target.value)}
+                maxLength={50}
+                className={`z-10 bg-white ${i<1 ? i % 2 ? "rotate-[-1.2deg]" : "rotate-1" : ""}`}
+              ></input>
+            );
+          })}
         </div>
-    )
-}
+
+        <div className="w-1/2 mx-auto mt-10 mb-5">
+          <ConfirmButton
+            color="primary"
+            type="submit"
+            customClasses="w-full -rotate-1 py-4 md:py-3 text-3xl md:text-4xl"
+          >
+            Invia
+          </ConfirmButton>
+        </div>
+      </form>
+    </div>
+  );
+};
+
 export default QuestionCard;
