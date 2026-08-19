58032c70bfc21e2cc754af3dfb61dc59a3099cfb
Luca <lugenba@gmail.com>
2026-08-19 12:58:01 +0200

    eliminata funzione getProfileStats da auth.controller

--- backend/src/controllers/Auth.controller.js
+++ backend/src/controllers/Auth.controller.js
@@ -264,65 +264,11 @@ async function updateUser(req, res){
   }
 }
 
-/**
- * Endpoint GET /profile/stats
- * Recupera le statistiche dell'utente e le ultime 10 partite giocate
- */
-async function getProfileStats(req, res) {
-  try {
-    //*cerco l'utetne
-    const user = await User.findById(req.userId);
-    if (!user) {
-      return res.status(404).json({ message: "Utente non trovato." });
-    }
-
-    //* calcolo winRate
-    let winRate = "0.00";
-    if (user.stats.gamesPlayed > 0) {
-      winRate = ((user.stats.gamesWon / user.stats.gamesPlayed) * 100).toFixed(2);
-    }
-
-    //* recupero le ultime 10 partite giocate dall'utente
-    const recentGames = await Game.find({ "players.user": req.userId })
-      .sort({ createdAt: -1 }) //? Ordina dalla più recente alla più vecchia
-      .limit(10);
-
-    //* cerco l'utente loggato dentro l'arrey dei giocatori della partita
-    const formattedGames = recentGames.map((game) => {
-      const me = game.players.find(
-        (p) => p.user.toString() === req.userId.toString()
-      );
-      
-      return {
-        id: game._id,
-        title: `STANZA DI ${game.hostUsername ? game.hostUsername.toUpperCase() : "SCONOSCIUTO"}`,
-        players: game.players.length,
-        maxPlayers: game.maxPlayers,
-        isWinner: me ? me.isWinner : false
-      };
-    });
-
-    res.status(200).json({
-      stats: {
-        points: user.stats.points,
-        gamesWon: user.stats.gamesWon,
-        gamesPlayed: user.stats.gamesPlayed,
-        winRate: winRate
-    },
-    games: formattedGames
-});
-
-  } catch (error) {
-    console.error("Errore recupero statistiche:", error);
-    return res.status(500).json({ message: "Impossibile caricare il profilo in questo momento." });
-  }
-}
 
 module.exports = {
   login,
   register,
   logout,
   refresh,
-  updateUser,
-  getProfileStats
+  updateUser
 };
