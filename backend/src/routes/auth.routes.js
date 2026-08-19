263020b744fecb445ad533dbf84c0dc0d4c009ba
Luca <lugenba@gmail.com>
2026-08-19 12:58:59 +0200

    rimossa rout da auth.routes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -9,6 +9,5 @@ router.post("/register", AuthController.register);
 router.post("/logout", AuthController.logout)
 router.post("/refresh", AuthController.refresh)
 router.put("/profile", isAuth, AuthController.updateUser)
-router.get("/profile/stats", isAuth, AuthController.getProfileStats);
 
 module.exports = router;
