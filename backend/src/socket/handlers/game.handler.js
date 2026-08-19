70209ae6cd086cd440778d7fcde55c3433b55a04
Jonathan <jonathan-caputo@hotmail.com>
2026-08-19 13:52:43 +0200

    Risolto gestione errori, implementato matchLefts
    Finalmente... direi dopo ore di debugging
    
    Co-Authored-By: defeangelica <234227725+defeangelica@users.noreply.github.com>

--- backend/src/socket/handlers/game.handler.js
+++ backend/src/socket/handlers/game.handler.js
@@ -1,4 +1,4 @@
-const { saveAnswers } = require("../../services/game.service");
+const { saveAnswers, calculateMatchLefts } = require("../../services/game.service");
 const { getLobby } = require("../../store/lobbyStore")
 const { serializeLobby } = require("../../store/lobbyStore");
 
@@ -15,24 +15,20 @@ function gameHandlers(io, socket) {
       return callback({ error: "Stanza non trovata" });
     }
 
-    let myMatches
     try{
-         myMatches = saveAnswers(lobby, socket, matchIndex, answers);
+        saveAnswers(lobby, socket, matchIndex, answers);
     }catch(err){
-        return callback({ error: err });
+        return callback({ error: err.message });
     }
-     
-    return callback({
-        lobby: serializeLobby(lobby),
-        myMatches: myMatches
-    })
-
-    const currentRound = lobby.rounds.get(lobby.currentRound)
-    const matchCompleted = currentRound.filter(m => m.p1.answers.length >= 1 && m.p2.answers.length >= 1)
 
      socket.to(lobby.code).emit("game:player_answered", {
       lobby: serializeLobby(lobby),
-      matchLefts: currentRound.length - matchCompleted
+      matchLefts: calculateMatchLefts(lobby),
+    })
+
+    return callback({
+        lobby: serializeLobby(lobby),
+        matchLefts: calculateMatchLefts(lobby),
     })
 
     //TODO: se matchLeft = 0... chiudi prima
