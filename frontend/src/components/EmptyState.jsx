5734e95eed08bbf7177a72f2647b750531c8b149
Jonathan <jonathan-caputo@hotmail.com>
2026-08-19 13:51:45 +0200

    P convertito in div...
    anche se doveva essere children
    anche se era meglio p, ma poi non avrei potuto mettere altro all'interno... vabbè

--- frontend/src/components/EmptyState.jsx
+++ frontend/src/components/EmptyState.jsx
@@ -1,9 +1,9 @@
 const EmptyState = ({ message = "Nessun contenuto disponibile" }) => {
   return (
     <div className="w-full flex justify-center py-10">
-      <p className="text-xl md:text-xl font-extrabold text-white uppercase -rotate-1 bg-black/20 px-8 py-2 rounded border-3 border-transparent border-dashed text-center">
+      <div className="text-xl md:text-xl font-extrabold text-white uppercase -rotate-1 bg-black/20 px-8 py-2 rounded border-3 border-transparent border-dashed text-center">
         {message}
-      </p>
+      </div>
     </div>
   );
 };
