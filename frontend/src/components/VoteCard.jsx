e7fd8580019229769dd48a6afdd4c9bd47747494
Jonathan <jonathan-caputo@hotmail.com>
2026-08-19 19:22:46 +0200

    🔧 Voting Card rinnovata con i dati del backend
    Ci siamo quasi, ora vanno aggiunti i punteggi
    
    Co-Authored-By: defeangelica <234227725+defeangelica@users.noreply.github.com>

--- frontend/src/components/VoteCard.jsx
+++ frontend/src/components/VoteCard.jsx
@@ -1,44 +1,77 @@
 import ConfirmButton from "./ConfirmButton";
 import { Icon } from "@iconify/react";
 
-const VoteCard = ({ username, phrases = [], host=false, isSelected, onSelect }) => {
-    return(
-        <div className="relative m-8 bg-white font-primary font-extrabold flex flex-col items-start pt-12 pb-6 px-4 md:py-10 md:px-8 border-3 border-neroNonNero shadow-buttons hover:-translate-y-1 transition-transform">
-            
-            <div className="absolute -top-6 left-6 flex items-center z-10">
-                <div className="w-12 h-12 rounded-full border-3 border-neroNonNero flex items-center justify-center overflow-hidden z-10 ">
-                    <img 
-                        src={`https://api.dicebear.com/10.x/critters/svg?tags=animation&seed=${username}`} 
-                        alt={`Avatar di ${username}`}
-                        className="w-full h-full object-cover"
-                    />
-                </div>
-                <div className="bg-neroNonNero text-white flex items-center gap-1 font-extrabold text-xs uppercase px-3 py-1.5 -ml-3 pl-5 rounded-r-lg border-2 border-neroNonNero">
-                    @{username}
-                    {host && (  
-                        <Icon icon="mdi:crown" className="text-[#FFD13B] text-sm md:text-base" />
-                    )}
-                </div>
-            </div>
-            <div className="flex flex-col gap-4 w-full mt-2">
-                {phrases.map((phrase, index) => (
-                    <span key={index} className="text-neroNonNero italic text-lg md:text-xl font-extrabold leading-tight">
-                        "{phrase}"
-                    </span>
-                ))}
-            </div>
-            <div className="w-[92%] mx-auto md:w-full mt-5">
-                <ConfirmButton 
-                    color="secondary"
-                    onClick={onSelect} 
-                    disabled={isSelected}
-                    customClasses="w-full py-4 md:py-5 text-xl md:text-2xl"
-                >
-                    {isSelected ? "GIÀ VOTATO" : "VOTA"}
-                </ConfirmButton>
-            </div>
+const MiniPlayer = ({ username, id, hostId }) => {
+  return (
+    <>
+      <div className="w-12 h-12 rounded-full border-3 border-neroNonNero flex items-center justify-center overflow-hidden z-10 ">
+        <img
+          src={`https://api.dicebear.com/10.x/critters/svg?tags=animation&seed=${id}`}
+          alt={`Avatar di ${username}`}
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="bg-neroNonNero text-white flex items-center gap-1 font-extrabold text-xs uppercase px-3 py-1.5 -ml-3 pl-5 rounded-r-lg border-2 border-neroNonNero">
+        @{username}
+        {id === hostId && (
+          <Icon
+            icon="mdi:crown"
+            className="text-[#FFD13B] text-sm md:text-base"
+          />
+        )}
+      </div>
+    </>
+  );
+};
+
+const VoteCard = ({
+  id,
+  username,
+  canVote,
+  hostId,
+  answers = [],
+  votedBy,
+  score,
+}) => {
+  answers = answers.filter((answer) => answer.trim() !== "");
+
+  return (
+    <div className="flex-1 relative bg-white font-primary font-extrabold flex flex-col justify-between min-h-40 pt-12 pb-6 px-4 md:py-10 md:px-8 border-3 border-neroNonNero shadow-buttons hover:-translate-y-1 transition-transform">
+      <div className="absolute -top-6 left-6 flex items-center z-10">
+        <MiniPlayer username={username} id={id} hostId={hostId} />
+      </div>
+      <div className="flex flex-1 flex-col justify-center my-auto gap-4 w-full text-neroNonNero text-lg md:text-xl font-extrabold leading-tight">
+        {answers.length > 0 ? answers.map((answer, index) => (
+          <span
+            key={"answer-" + id+ "-"+index}
+            className="italic"
+          >
+            "{answer}"
+          </span>
+        )) : <span
+            className="flex flex-col items-center justify-center md:mt-3"
+          >
+            Nessuna risposta
+            <Icon icon="glyphs-poly:sad-cry" width="64"/>
+          </span>}
+      </div>
+      {canVote || answers.length != 0 &&  (
+        <div className="w-[92%] mx-auto md:w-full mt-5">
+          <ConfirmButton
+            color="secondary"
+            customClasses="w-full py-4 md:py-5 text-xl md:text-2xl"
+          >
+            VOTA
+          </ConfirmButton>
+          <div className="flex gap-2 mt-3 md:mt-5 flex-wrap">
+          <MiniPlayer username={"dsds"} id={id} hostId={hostId} />
+          <MiniPlayer username={"dsds"} id={id} hostId={hostId} />
+          <MiniPlayer username={"dsds"} id={id} hostId={hostId} />
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default VoteCard;
