fbfb0ae81313f2034b40648eee3fd701396775b4
Jonathan <jonathan-caputo@hotmail.com>
2026-08-19 19:21:17 +0200

    Separata la QuestionCard in QuestionForm e Question
    La question viene sempre visualizzata praticamente... più ordinato
    
    Co-Authored-By: defeangelica <234227725+defeangelica@users.noreply.github.com>

--- frontend/src/components/QuestionForm.jsx
+++ frontend/src/components/QuestionForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import ConfirmButton from "./ConfirmButton";
 
-const QuestionCard = ({ question, onSubmit, isFinal }) => {
+const QuestionForm = ({ onSubmit, isFinal }) => {
   const [answers, setAnswers] = useState(["", "", ""]);
 
   function handleChange(index, value) {
@@ -9,21 +9,8 @@ const QuestionCard = ({ question, onSubmit, isFinal }) => {
   }
   
   return (
-    <div className="flex flex-col items-center mt-5 px-4 gap-3 w-full mx-auto md:max-w-3xl">
-      <div className="relative w-full bg-white shadow-buttons border-3 border-neroNonNero px-6 py-7 md:px-20 md:py-10 rounded-2xl select-none">
-        <h2 className="text-2xl md:text-3xl font-extrabold text-center">
-          {question.text}
-        </h2>
-        <p className="absolute uppercase -top-3 md:-top-5 -left-4 bg-[#FFDF9A] shadow-buttons text-xs px-3 py-1 font-bold -rotate-2 select-none md:text-base">
-          domanda flash
-        </p>
-        <p className="absolute uppercase -bottom-3 -right-2 bg-[#C4C0FF] shadow-buttons text-xs px-3 py-1 rotate-3 text-[#2100A4] font-bold select-none md:text-base">
-          non mentire!
-        </p>
-      </div>
-
       <form
-        className="w-[80%]"
+        className="w-[80%] md:w-[70%] mx-auto"
         onSubmit={(e) => {
           e.preventDefault();
           onSubmit(answers);
@@ -56,8 +43,7 @@ const QuestionCard = ({ question, onSubmit, isFinal }) => {
           </ConfirmButton>
         </div>
       </form>
-    </div>
   );
 };
 
-export default QuestionCard;
+export default QuestionForm;
