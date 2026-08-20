4cbfc586be216e042306796b91bcadad99ff1a19
Jonathan <jonathan-caputo@hotmail.com>
2026-08-20 19:30:25 +0200

    Sistamato responsive resultPlayer

--- frontend/src/components/ResultPlayer.jsx
+++ frontend/src/components/ResultPlayer.jsx
@@ -1,18 +1,18 @@
-const ResultPlayer = ({ username, position, points }) => {
+const ResultPlayer = ({ username, position, points, className }) => {
   return (
     <div
-      className={`flex items-center bg-white border-3 border-neroNonNero shadow-buttons py-2 md:py-5 px-5 font-primary`}
+      className={`flex items-center bg-white border-3 border-neroNonNero shadow-buttons py-2 md:py-5 px-5 font-primary ${className}`}
     >
       <div className="flex items-center grow gap-3">
         <span className="text-black/40 text-2xl font-bold mr-6">
           {position}
         </span>
         <img
-          className="w-16 md:w-8 md:h-8 rounded-full border-3 border-neroNonNero shadow-buttons md:shadow-none"
+          className="w-14 md:w-10 md:h-10 rounded-full border-3 border-neroNonNero shadow-buttons md:shadow-none"
           src={`https://api.dicebear.com/10.x/critters/svg?tags=animation&seed=${username}`}
           alt={`Avatar di ${username}`}
         />
-        <span className="font-bold text-xs md:text-xl mt-3 md:mt-0 uppercase tracking-wide truncate max-w-full text-white md:text-black">
+        <span className="font-bold text-xl ml-3 uppercase tracking-wide truncate max-w-full md:text-black">
           {username}
         </span>
       </div>
