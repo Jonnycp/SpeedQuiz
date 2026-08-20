71c38e5b07b926f35fda7c764c50cdbf18f67da2
Jonathan <jonathan-caputo@hotmail.com>
2026-08-20 20:49:15 +0200

    Funzioni di salvataggio game in db
    Con errori... purtroppo
    
    Co-Authored-By: defeangelica <234227725+defeangelica@users.noreply.github.com>

---
+++ backend/src/services/saving.service.js
@@ -0,0 +1,60 @@
+const Game = require("../models/Game.js");
+const User = require("../models/User.js");
+const Question = require("../models/Question.js");
+
+async function saveGameResult(lobby) {
+  const winnerPlayer = [...lobby.players.values()].reduce((prev, current) => {
+    return prev.score > current.score ? prev : current;
+  });
+
+  //* Salvataggio partita nel database
+  const game = new Game({
+    hostId: lobby.hostId,
+    hostUsername: lobby.hostUsername,
+    config: {
+      nRounds: lobby.config.rounds,
+      answerTime: lobby.config.answerTimeMs / 1000,
+    },
+    players: [...lobby.players.values()].map((player) => ({
+      user: player.id,
+      username: player.username,
+      score: player.score,
+      isWinner: winnerPlayer.id === player.id,
+    })),
+  });
+  await game.save();
+
+  //* UPDATE STATS UTENTE
+
+  async function updateUser(player, winnerPlayer) {
+    await User.findByIdAndUpdate(player.id, {
+      $inc: {
+        "stats.gamesPlayed": 1,
+        "stats.gamesWon": winnerPlayer.id === player.id ? 1 : 0,
+        "stats.points": player.score,
+      },
+    });
+  }
+
+  await Promise.all([...lobby.players.values()].map((p) => updateUser(p, winnerPlayer)));
+
+  return game;
+}
+
+async function updateUsedQuestions(lobby) {
+    const rounds = [...lobby.rounds.values()]
+    const questions = rounds.map(r => r.question._id)
+
+    questions.forEach(async (questionId) => {
+        await Question.findByIdAndUpdate(questionId, {
+            $inc: { timesUsed: 1 }
+        });
+    });
+}
+
+async function savingENDGame(lobby){
+    updateUsedQuestions(lobby)
+    return saveGameResult(lobby)
+}
+
+module.exports = { saveGameResult, updateUsedQuestions, savingENDGame }
