dc2e86c5cde1ced92dc0d57169bb5d6c6969f31c
defeangelica <a.defeudis3@studenti.poliba.it>
2026-08-21 14:41:23 +0200

    update score.service: punteggio 0 se player non scrive nessuna risposta

--- backend/src/services/score.service.js
+++ backend/src/services/score.service.js
@@ -6,15 +6,18 @@ function calculateMatchWinner(lobby, match){
     const votedForP1 = match.p1.votedBy.length;
     const votedForP2 = match.p2.votedBy.length;
 
-    const scoreP1 = calculateScore(lobby.currentRound, votedForP1, lobby.players.size-2)
-    const scoreP2 = calculateScore(lobby.currentRound, votedForP2, lobby.players.size-2)
+    //*  calcola lo score, solo se il player ha scritto almeno una risposta
+    const scoreP1 = match.p1.answers.length > 0 ? calculateScore(lobby.currentRound, votedForP1, lobby.players.size-2) : 0;
+    const scoreP2 = match.p2.answers.length > 0 ? calculateScore(lobby.currentRound, votedForP2, lobby.players.size-2) : 0;
 
+    //* aggiornamento score anche in lobby
     match.p1.score = scoreP1;
     match.p2.score = scoreP2;
 
     lobby.players.get(match.p1.id).score += scoreP1;
     lobby.players.get(match.p2.id).score += scoreP2;
 
+    //* determinazione del vincitore 
     if(scoreP1 > scoreP2){
         match.p1.isWinner = true;
         return match.p1;
@@ -28,8 +31,7 @@ function calculateMatchWinner(lobby, match){
     }
 }
 
-//* Funzione per calcolare punteggi
-// Round*10 + %preferenza + bonus (se 100%)
+//* Funzione per calcolare punteggi: round*10 + %preferenza + bonus (se 100%)
 function calculateScore(indexRound, votedFor, voters){
     const perc = Math.floor(votedFor/voters * 100)
     const bonus = perc === 100 ? Number(process.env.BONUS) : 0;
